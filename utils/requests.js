92c09a90c10c54e8dddbd4301fb6bd1628b35dfb
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-11-04 00:19:31 +0530

    Added main sections and connected to apis

--- utils/requests.js
+++ utils/requests.js
@@ -13,46 +13,46 @@ export default{
 
     fetchActionMovies:{
         title: 'Action',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=28`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=28`
     },
 
     fetchComedyMovies:{
         title: 'Comedy',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=35`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=35`
     },
 
     fetchHorrorMovies:{
         title: 'Horror',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=27`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=27`
     },
 
     fetchRomanceMovies:{
         title: 'Romance',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=10749`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10749`
     },
 
     fetchMystery:{
         title: 'Mystery',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=9648`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=9648`
     },
 
     fetchSciFi:{
         title: 'Sci-Fi',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=878`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=878`
     },
 
     fetchWestern:{
         title: 'Western',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=37`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=37`
     },
 
     fetchAnimation:{
         title: 'Animation',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=16`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=16`
     },
 
     fetchTV:{
         title: 'TV Movie',
-        url: `/discover/movie?api_key=${API_KEY}&width_genres=10770`
+        url: `/discover/movie?api_key=${API_KEY}&with_genres=10770`
     }
 }
