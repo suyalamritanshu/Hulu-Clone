c7729162820c550736d67e90dfedd991b9ee1b1c
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-11-03 23:29:48 +0530

    Header and navbar added

---
+++ components/Thumbnail.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import Image from 'next/image';
+
+function Thumbnail({result}) {
+    
+    return (
+        <div >
+           <Image
+             layout='responsive'
+             src={}
+             height={1080}
+             width={1920}
+
+           />
+        </div>
+    )
+}
+
+export default Thumbnail;
--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
+import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({results}) {
+  
   return (
     <div>
       <Head>
@@ -9,14 +13,29 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      
       {/* Header Components */}
 
-      <Header/>
+      <Header />
 
       {/* Navbar Components */}
+      <Nav />
 
       {/* Results Components */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
