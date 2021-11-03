0104c3c331b1276ee322137c284dd652bede48ae
suyalamritanshu <amritanshu.suyall2@gmail.com>
2021-11-03 09:47:41 +0530

    Basic setup

--- components/Header.js
+++ components/Header.js
@@ -1,11 +1,30 @@
-import React from 'react'
+import React from "react";
+import Image from "next/image";
+import HeaderItem from "./HeaderItem";
+import {
+  BadgeCheckIcon,
+  CollectionIcon,
+  HomeIcon,
+  LightningButton,
+  SearchIcon,
+  UserIcon,
+} from "@heroicons/react/outline";
 
 function Header() {
-    return (
-        <header className="">
-            <h1>This is the header</h1>
-        </header>
-    )
+  return (
+    <header className="">
+      <div>
+        <HeaderItem title='HOME' Icon={HomeIcon} />
+      </div>
+
+      <Image
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
 }
 
-export default Header
+export default Header;
---
+++ components/HeaderItem.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+function HeaderItem({Icon, title}) {
+    return (
+        <div>
+            <Icon className="h-8 mb-1" />
+            <p>{title}</p>
+        </div>
+    );
+}
+
+export default HeaderItem;
